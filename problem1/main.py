4a8e02225e7f55c1d1c4661dbbaa36a5e891e755
fitri <fitrimuslikhah7@gmail.com>
2024-05-05 13:34:57 +0700

    problem 1

--- problem1/main.py
+++ problem1/main.py
@@ -2,6 +2,17 @@
 student_score = 80
 
 # Process: Your Solution Code Here
-
+def convert_to_grade(score):
+    if score >= 80:
+        return "A"
+    elif score >= 65:
+        return "B+"
+    elif score >= 50:
+        return "B"
+    elif score >= 35:
+        return "C"
+    else:
+        return "D"
 
 # Output "Nilai A"
+print("Nilai", convert_to_grade(student_score));
