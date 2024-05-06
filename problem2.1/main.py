a46638027cfef7714d1bacbd4e2b538262470180
fitri <fitrimuslikhah7@gmail.com>
2024-05-06 10:11:09 +0700

    problem 2

--- problem2.1/main.py
+++ problem2.1/main.py
@@ -1,1 +1,13 @@
 bilangan = int(input())
+
+print("Faktor dari", bilangan, "adalah:")
+for i in range(1, bilangan + 1):
+    if bilangan % i == 0:
+        print(i)
+
+
+print("Faktor dari", bilangan, "adalah:")
+for i in range(1, bilangan + 1):
+    if bilangan % i == 0:
+        print(i)
+
