d13a67240f92449ce99e380f8663a86664c4ded9
fitri <fitrimuslikhah7@gmail.com>
2024-05-06 15:11:39 +0700

    problem 4

--- problem4/main.py
+++ problem4/main.py
@@ -1,6 +1,10 @@
 def palindrome(input_string):
-    return 'error response'
+    #return 'error response'
 
+ # hapus spasi input string dan mengonversinya menjadi huruf kecil
+    input_string = input_string.replace(" ", "").lower()
+ # Memeriksa apakah string dan string yang dibalik sama
+    return input_string == input_string[::-1]
 
 if __name__ == '__main__':
     print(palindrome("civic")) # True
@@ -8,3 +12,4 @@ if __name__ == '__main__':
     print(palindrome("kasur rusak")) # True
     print(palindrome("kupu-kupu")) # False
     print(palindrome("lion")) # False
+
