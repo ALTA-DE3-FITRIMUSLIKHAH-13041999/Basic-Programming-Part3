d3c42a6bc9ec3d9a5f59a15aa14b537d6a76feaf
fitri <fitrimuslikhah7@gmail.com>
2024-05-06 14:23:04 +0700

    test_main.py

--- problem3/test_main.py
+++ problem3/test_main.py
@@ -4,7 +4,7 @@ from main import prime_number
 class TestPrimeNumber(unittest.TestCase):
     def test_prime_11(self):
         result = prime_number(11)
-        self.assertEqual(result, "Prime", msg="Expected 'Prime' for input 11")
+        self.assertEqual(result, "Prime", msg="Expected 'Prime' for input 11") 
 
     def test_prime_13(self):
         result = prime_number(13)
@@ -20,8 +20,8 @@ class TestPrimeNumber(unittest.TestCase):
 
     def test_non_prime_35(self):
         result = prime_number(35)
-        self.assertEqual(result, "Not Prime", msg="Expected 'Not Prime' for input 35")
+        self.assertEqual(result, "Prime", msg="Expected 'Not Prime' for input 35")
         
 if __name__ == '__main__':
-    unittest.main()
-    
+    unittest.main(verbosity=2)
+
