d8062405bee3140a6194615843cae8ff04bc2a3d
fitri <fitrimuslikhah7@gmail.com>
2024-05-06 10:47:48 +0700

    problem 2.2

--- problem2.2/main.py
+++ problem2.2/main.py
@@ -1,1 +1,10 @@
-bilangan = int(input())
+bilangan = int(input("masukkan bilangan: "))
+
+faktor = []
+for i in range(1, bilangan + 1):
+    if bilangan % i == 0:
+        faktor.append(i)
+
+print("faktor", bilangan, "adalah:")
+for i in range(len(faktor) -1, -1, -1):
+    print(faktor[i])
